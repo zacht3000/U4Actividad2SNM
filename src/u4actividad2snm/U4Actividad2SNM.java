4482a7c412aa06c70212e6aa2b12a36a351d3600
zacht3000 <zacht3000@DESKTOP-ULVUR77>
2019-12-05 00:43:36 +0100

    Se han quitado comentarios inecesarios y se ha creado unas constantes.

--- src/u4actividad2snm/U4Actividad2SNM.java
+++ src/u4actividad2snm/U4Actividad2SNM.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package u4actividad2snm;
 
 /**
  *
- * @author zacht3000
+ * @author Santiago Naranjo Marcillo
  */
 public class U4Actividad2SNM {
 
-    /**
-     * @param args the command line arguments
-     */
+    static final int JUGADORES = 4;
+    static final int REPARTICION = 4;
+    
     public static void main(String[] args) {
         U4Actividad2SNM programa = new U4Actividad2SNM();
         programa.inicio();
