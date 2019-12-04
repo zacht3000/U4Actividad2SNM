f43cc41d14f318aa69f487706a4dc75be7bc42a7
zacht3000 <zacht3000@DESKTOP-ULVUR77>
2019-12-05 00:22:44 +0100

    Creado el incio del juego.

---
+++ src/u4actividad2snm/U4Actividad2SNM.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package u4actividad2snm;
+
+/**
+ *
+ * @author zacht3000
+ */
+public class U4Actividad2SNM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        U4Actividad2SNM programa = new U4Actividad2SNM();
+        programa.inicio();
+    }
+
+    public void inicio() {
+        
+    }
+}
